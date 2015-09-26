1ea311a5708efba27ad58c18d899a9c7bafcf93b
Eugene Korchagin <evgprogger@gmail.com>
2015-09-26 05:44:13 +0300

    Login-window

--- js/custom.js
+++ js/custom.js
@@ -2,11 +2,13 @@
  * Created by macbook on 26.09.15.
  */
 function showLogin(){
+    $(".fade").css("display", "block");
+    $(".fade").css("opacity","1");
     $(".login-form").css("display", "block");
-    $(".fade").css("display","block");
 };
 
 function closeLogin(){
+    $(".fade").css("display", "none");
+    $(".fade").css("opacity","0");
     $(".login-form").css("display", "none");
-    $(".fade").css("display","none");
 };
