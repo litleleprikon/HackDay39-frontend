60677a4e9bd70c227637e6965870235eaf781593
Eugene Korchagin <evgprogger@gmail.com>
2015-09-26 05:04:59 +0300

    Home

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+/**
+ * Created by macbook on 26.09.15.
+ */
+function showLogin(){
+    $(".login-form").css("display", "block");
+    $(".fade").css("display","block");
+};
+
+function closeLogin(){
+    $(".login-form").css("display", "none");
+    $(".fade").css("display","none");
+};
